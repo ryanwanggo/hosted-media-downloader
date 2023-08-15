0bb19d3ce29562364931ba16c9486334e40fd512
ryanwanggo <shihuan.wang.vobileinc.com>
2023-08-15 13:26:40 -0400

    put the files into the src folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { mp4Filedownloader } from "./mp4-filedownloader";
 import { m3u8FileDownloader } from "./m3u8-fileDownloader";
 
-// mp4Filedownloader()
-m3u8FileDownloader();
+mp4Filedownloader()
+// m3u8FileDownloader();
